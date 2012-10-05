b0ce80ffe142805cedd6b39d337311e40b9cfc14
ka2er <personseb+git@gmail.com>
2012-10-06 00:11:17 +0200

    add API endpoints api lang list api sub searcj

--- test/test.js
+++ test/test.js
@@ -6,15 +6,15 @@
  */
 
 var assert = require("assert");
-	OS = require("../lib/subdb.js");
+	SubDb = require("../lib/subdb.js");
 
 describe('Subdb', function() {
 
 	describe('#computeHash()', function(){
 
 		it('should return a subdb hash of the dexter sample file ', function(done){
-			var os = new OS();
-			os.computeHash(process.cwd()+'/test/dexter.mp4', function(err, size){
+			var subdb = new SubDb();
+			subdb.computeHash(process.cwd()+'/test/dexter.mp4', function(err, size){
 				if (err) return done(err);
 				assert.equal(size, 'ffd8d4aa68033dc03d1c8ef373b9028c');
 				done();
@@ -22,8 +22,8 @@ describe('Subdb', function() {
 		});
 
 		it('should return a opensubtitles hash of the justifier sample file ', function(done){
-			var os = new OS();
-			os.computeHash(process.cwd()+'/test/justified.mp4', function(err, size){
+			var subdb = new SubDb();
+			subdb.computeHash(process.cwd()+'/test/justified.mp4', function(err, size){
 				if (err) return done(err);
 				assert.equal(size, 'edc1981d6459c6111fe36205b4aff6c2');
 				done();
@@ -32,8 +32,8 @@ describe('Subdb', function() {
 
 
 		it('should return a 128bits length opensubtitles hash of the movie file ', function(done){
-			var os = new OS();
-			os.computeHash(process.cwd()+'/test/justified.mp4', function(err, size){
+			var subdb = new SubDb();
+			subdb.computeHash(process.cwd()+'/test/justified.mp4', function(err, size){
 				if (err) return done(err);
 				assert.equal(size.length, 'edc1981d6459c6111fe36205b4aff6c2'.length);
 				done();
@@ -41,19 +41,33 @@ describe('Subdb', function() {
 		});
 
 	});
-/*
-	describe('#checkMovieHash', function() {
-		it('should identify justified trailer', function(done) {
-			this.timeout(4000);
-			var os = new OS();
-			os.checkMovieHash(['edc1981d6459c6111fe36205b4aff6c2'], function(err, res) {
-				if(err) return done(err);
 
-				assert.equal(res.data['edc1981d6459c6111fe36205b4aff6c2'], '???');
-				done();
+	describe('api', function() {
+
+		describe('#available_languages', function(){
+			it('should list available languages', function(done) {
+				//this.timeout(4000);
+				var subdb = new SubDb();
+				subdb.api.available_languages(function(err, res){
+					if(err) return done(err);
+
+					assert.equal(res.join(','), ["af","cs","da","de","en","es","fi","fr","hu","id","it","la","nl","no","oc","pl","pt","ro","ru","sl","sr","sv","tr"].join(','));
+					done();
+				});
+			});
+		});
+
+		describe('#search_subtitles', function(){
+			it('should search for subtitles', function(done) {
+				var subdb = new SubDb();
+				subdb.api.search_subtitles('edc1981d6459c6111fe36205b4aff6c2', function(err, res){
+					if(err) return done(err);
+
+					assert.equal(res.join(','), ["en","fr","it","pt"].join(','));
+					done();
+				});
 			});
 		});
 	});
-*/
 });
 
